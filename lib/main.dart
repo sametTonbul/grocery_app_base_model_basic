595da2f189c4cb92a769d03eb82cbf07916c2e97
Samet Tonbul <stsamettonbul@gmail.com>
2022-12-11 14:24:51 +0300

    grocery_app_base

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/item_model.dart';
+import 'screens/opening.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: IntroScreen(),
+      ),
+    );
+  }
+}
